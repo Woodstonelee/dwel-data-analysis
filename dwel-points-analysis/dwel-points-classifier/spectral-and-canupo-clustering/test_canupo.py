ac5facd1e5716b4f30508fce2f280e9a16c533d8
Zhan Li <zhanli1986@gmail.com>
2016-03-21 19:05:40 -0400

    stop tracking shell scripts for batch processisng.

--- dwel-points-analysis/dwel-points-classifier/spectral-and-canupo-clustering/test_canupo.py
+++ dwel-points-analysis/dwel-points-classifier/spectral-and-canupo-clustering/test_canupo.py
@@ -1,6 +1,11 @@
+import sys
+
 import canupo
 
-mscfile = canupo.MSCFile("/projectnb/echidna/lidar/DWEL_Processing/HF2014/Hardwood20140919/spectral-points-by-union/HFHD_20140919_dual_points/HFHD_20140919_C_dual_cube_bsfix_pxc_update_atp2_ptcl_points_az10.msc")
+sys.settrace
+
+# mscfile = canupo.MSCFile("/projectnb/echidna/lidar/DWEL_Processing/HF2014/Hardwood20140919/spectral-points-by-union/HFHD_20140919_dual_points/HFHD_20140919_C_dual_cube_bsfix_pxc_update_atp2_ptcl_points_az10.msc")
+mscfile = canupo.MSCFile("/projectnb/echidna/lidar/DWEL_Processing/HF2014/Hardwood20140919/spectral-points-by-union/HFHD_20140919_dual_points-sacc/HFHD_20140919_W_dual_cube_bsfix_pxc_update_atp2_ptcl_points.msc")
 
 # scales = canupo.FloatVec()
 # scales[:] = []
@@ -8,7 +13,7 @@ mscfile = canupo.MSCFile("/projectnb/echidna/lidar/DWEL_Processing/HF2014/Hardwo
 
 # print canupo.read_msc_header(mscfile, scales, ptnparams)
 npts, scales, nparams = canupo.read_msc_header(mscfile)
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 data = canupo.FloatVec()
 # canupo.read_msc_data(mscfile, len(scales), npts, data, nparams, True)
 canupo.read_msc_data(mscfile, len(scales), npts, data, nparams)
