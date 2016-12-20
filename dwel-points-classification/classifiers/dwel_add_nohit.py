67d93eb07b97560b95580f23452f1f2824105dc3
Zhan Li <zhanli1986@gmail.com>
2016-12-20 16:25:10 -0500

    Change folder name of source files for classification accuracy assessment. Start coding a python script to add nohit points back to the classified point cloud which does not include nohit points.

---
+++ dwel-points-classification/classifiers/dwel_add_nohit.py
@@ -0,0 +1,27 @@
+"""Add no-hit points in the original DWEL point cloud before
+classification back to the classified point cloud which has been
+generated from a point cloud without no-hit points.
+
+Zhan Li, zhanli86@bu.edu
+Created: Tue Oct 25 22:14:25 EDT 2016
+"""
+
+import sys
+import argparse
+
+import numpy as np
+
+import dwel_points_utils as dpu
+
+def addNoHits(in_file, nohit_file, out_file):
+    pass
+
+def getCmdArgs():
+    pass
+
+def main(cmdargs):
+    pass
+
+if __name__ == '__main__':
+    cmdargs = getCmdArgs()
+    main(cmdargs)
